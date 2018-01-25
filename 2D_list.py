c201ca19764ff192cd7341f81922333f5bbb28fe
gamixaqua <yoaquayo@gmail.com>
2018-01-26 00:52:26 +0530

    Create 2D_list.py

---
+++ 2D_list.py
@@ -0,0 +1,7 @@
+m = int(input("Enter the number of row"))
+n=int(input("Enter the number of the coloumn"))
+multi_list=[[0 for col in range(n)]for row in range(m)]//initilize the list
+for i in range(m):
+    for j in range(n):
+        multi_list[i][j] = i*j;
+print(multi_list)
